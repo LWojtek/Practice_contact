7907d3546564106aa1cf8f1f936573ba55057565
Wojciech Lehmann <lehmann.wojciech@icloud.com>
2021-01-08 11:53:26 +0100

    completed

--- app.js
+++ app.js
@@ -1,20 +1,11 @@
-const logo = document.querySelector('.logo');
+/* ***************************************************** */
+/* ******* S T I C K Y  B O X  O N  S C R O L L  ******* */
+/* ***************************************************** */
+
+
+
 const tracker = document.querySelector('.track');
-let navigationLinks = document.querySelectorAll('nav ul li a');
-let nav = document.querySelector('nav');
-const square = document.querySelectorAll('.square');
-const monuntain1 = document.querySelector('#mountain1')
-const monuntain2 = document.querySelector('#mountain2')
-const monuntain3 = document.querySelector('#mountain3')
-const paragraph1 = document.querySelector('.paragraph1');
-const contactBtn = document.querySelector('.contact__btn');
-const paragraph = document.querySelector('.paragraph1');
-const h1 = document.querySelector('.h1');
 
-
-
-
-// article track scroll
 
 
 document.addEventListener('scroll', () => {
@@ -32,7 +23,15 @@ document.addEventListener('scroll', () => {
 })
 
 
-// navigation colors change on scroll 
+/* ***************************************************** */
+/* ** L O G O  B A C K G R O U N D   O N  S C R O L L ** */
+/* ***************************************************** */
+
+
+let navigationLinks = document.querySelectorAll('nav ul li a');
+let nav = document.querySelector('nav');
+const logo = document.querySelector('.logo');
+
 
 
 document.addEventListener('scroll', () => {
@@ -41,17 +40,32 @@ document.addEventListener('scroll', () => {
             logo.setAttribute('src', './assets/white-bg.svg')
             navigationLinks.forEach(el => el.style.color = 'var(--pri-den-gray-100)')
             nav.style.backgroundColor = 'rgba(255,255,255, .7)';
-            // square.forEach(el => el.style.animation = 'slide-in .5s ease-in forwards');
       } else {
             logo.setAttribute('src', './assets/dark-bg.png')
             navigationLinks.forEach(el => el.style.color = 'white')
             nav.style.backgroundColor = 'transparent';
             tracker.style.position = `relative`;
-            // square.forEach(el => el.style.animation = 'slide-back .5s ease-in forwards');
       }
 })
 
-// animation on scroll footer
+document.addEventListener('scroll', () => {
+
+      if (scrollY > 3300) {
+            logo.setAttribute('src', './assets/dark-bg.svg')
+            navigationLinks.forEach(el => el.style.color = 'white')
+            nav.style.backgroundColor = 'rgba(0,0,0, .7)';
+            nav.style.transition = '.5s';
+      }
+})
+
+/* ***************************************************** */
+/* ********** A N I M A T I N G  F O O T E R ******** ** */
+/* ***************************************************** */
+
+const monuntain1 = document.querySelector('#mountain1')
+const monuntain2 = document.querySelector('#mountain2')
+const monuntain3 = document.querySelector('#mountain3')
+
 
 document.addEventListener('scroll', () => {
       if (scrollY > 2700) {
@@ -66,30 +80,9 @@ document.addEventListener('scroll', () => {
 })
 
 
-
-document.addEventListener('scroll', () => {
-
-      if (scrollY > 2500) {
-            paragraph1.classList.add('animate-paragraph1');
-            contactBtn.classList.add('animate-contact');
-      } else {
-            paragraph1.classList.remove('animate-paragraph1');
-            contactBtn.classList.remove('animate-contact');
-      }
-})
-
-document.addEventListener('scroll', () => {
-
-      if (scrollY > 2900) {
-            logo.setAttribute('src', './assets/dark-bg.svg')
-            navigationLinks.forEach(el => el.style.color = 'white')
-            nav.style.backgroundColor = 'rgba(0,0,0, .7)';
-            nav.style.transition = '.5s';
-      }
-})
-
-
-// T E S T // 
+/* ***************************************************** */
+/* ** A N I M A T I N G  C A R D  B A C K G R O U N D ** */
+/* ***************************************************** */
 
 
 const test = document.querySelectorAll('#phase1');
@@ -102,7 +95,7 @@ const test4 = document.querySelectorAll('#phase5');
 
 document.addEventListener('scroll', () => {
 
-      if (scrollY > 1600) {
+      if (scrollY > 1600 && scrollY < 2000) {
             test.forEach(el => el.style.animation = 'slide-in .2s ease-in forwards');
       } else {
             test.forEach(el => el.style.animation = 'slide-back .2s ease-in forwards');
@@ -110,7 +103,7 @@ document.addEventListener('scroll', () => {
 })
 document.addEventListener('scroll', () => {
 
-      if (scrollY > 1850) {
+      if (scrollY > 1650 && scrollY < 2200) {
             test1.forEach(el => el.style.animation = 'slide-in .2s ease-in forwards');
       } else {
             test1.forEach(el => el.style.animation = 'slide-back .2s ease-in forwards');
@@ -118,7 +111,7 @@ document.addEventListener('scroll', () => {
 })
 document.addEventListener('scroll', () => {
 
-      if (scrollY > 2100) {
+      if (scrollY > 2100 && scrollY < 2800) {
             test2.forEach(el => el.style.animation = 'slide-in .2s ease-in forwards');
       } else {
             test2.forEach(el => el.style.animation = 'slide-back .2s ease-in forwards');
@@ -126,7 +119,7 @@ document.addEventListener('scroll', () => {
 })
 document.addEventListener('scroll', () => {
 
-      if (scrollY > 2450) {
+      if (scrollY > 2550 && scrollY < 2800) {
             test3.forEach(el => el.style.animation = 'slide-in .2s ease-in forwards');
       } else {
             test3.forEach(el => el.style.animation = 'slide-back .2s ease-in forwards');
@@ -138,5 +131,50 @@ document.addEventListener('scroll', () => {
             test4.forEach(el => el.style.animation = 'slide-in .2s ease-in forwards');
       } else {
             test4.forEach(el => el.style.animation = 'slide-back .2s ease-in forwards');
+      }
+
+})
+
+/* ***************************************************** */
+/* ********** A N I M A T I N G   T E X T S ************ */
+/* ***************************************************** */
+
+const paragraph1 = document.querySelector('.paragraph1');
+const contactBtn = document.querySelector('.contact__btn');
+const paragraph = document.querySelector('.paragraph');
+const h1 = document.querySelector('.h1');
+
+
+
+document.addEventListener('scroll', () => {
+
+      if (scrollY > 1100 && scrollY < 1800) {
+            paragraph.style.animation = 'from-bottom .3s ease-in forwards'
+            paragraph.style.animationDelay = '.5s';
+      } else {
+            paragraph.style.animation = 'to-bottom .3s ease-in forwards'
+            paragraph.style.animationDelay = '.5s';
+      }
+
+})
+
+document.addEventListener('scroll', () => {
+
+      if (scrollY > 1100 && scrollY < 1800) {
+            h1.style.animation = 'from-right .3s ease-in forwards'
+      } else {
+            h1.style.animation = 'to-right .3s ease-in forwards'
+      }
+
+})
+
+document.addEventListener('scroll', () => {
+
+      if (scrollY > 2500) {
+            paragraph1.classList.add('animate-paragraph1');
+            contactBtn.classList.add('animate-contact');
+      } else {
+            paragraph1.classList.remove('animate-paragraph1');
+            contactBtn.classList.remove('animate-contact');
       }
 })
